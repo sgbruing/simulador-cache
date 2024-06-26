46394dab28059e0355848ebf8d92fc2c2d8d7754
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2024-06-26 00:51:59 -0300

    ADDED: Processamento do arquivo de entrada e simulação da cache

--- main.c
+++ main.c
@@ -27,6 +27,77 @@ typedef struct {
     int politica_substituicao;
 } Cache;
 
+void extrair_endereco(uint32_t endereco, int *indice, int *tag, int bits_indice, int bits_deslocamento) {
+    int bits_tag = TAM_ENDERECO - bits_indice - bits_deslocamento;
+    *indice = (endereco >> bits_deslocamento) & ((1 << bits_indice) - 1);
+    *tag = endereco >> (bits_indice + bits_deslocamento);
+}
+
+void simular_cache(Cache *cache, uint32_t endereco, char operacao, int *cont_acertos, int *cont_falhas, int *leituras_memoria, int *escritas_memoria) {
+    int bits_deslocamento = __builtin_ctz(cache->tamanho_linha); //bit counting
+    int bits_indice = __builtin_ctz(cache->quantidade_conjuntos);
+    int indice, tag;
+    extrair_endereco(endereco, &indice, &tag, bits_indice, bits_deslocamento);
+
+    ConjuntoCache *conjunto = &cache->conjuntos[indice];
+    int acerto = 0;
+    int linha_vazia = -1;
+    int linha_substituir = 0;
+
+    // Verificar se há um acerto e encontrar uma linha vazia ou candidata para substituição
+    for (int i = 0; i < cache->linhas_por_conjunto; i++) {
+        if (conjunto->linhas[i].valido && conjunto->linhas[i].tag == tag) {
+            acerto = 1;
+            conjunto->linhas[i].contador_lru = 0;
+            conjunto->linhas[i].contador_lfu++;
+            if (operacao == 'W' && cache->politica_escrita == 1) {
+                conjunto->linhas[i].sujo = 1;
+            }
+            break;
+        }
+        if (!conjunto->linhas[i].valido && linha_vazia == -1) {
+            linha_vazia = i;
+        }
+        if (cache->politica_substituicao == 1 && conjunto->linhas[i].contador_lru > conjunto->linhas[linha_substituir].contador_lru) {
+            linha_substituir = i;
+        } else if (cache->politica_substituicao == 2 && conjunto->linhas[i].contador_lfu < conjunto->linhas[linha_substituir].contador_lfu) {
+            linha_substituir = i;
+        }
+    }
+
+    // Se houver acerto, atualizar o contador de acertos
+    if (acerto) {
+        (*cont_acertos)++;
+    } else {
+        // Lida com a falha
+        (*cont_falhas)++;
+        if (operacao == 'R') {
+            (*leituras_memoria)++;
+        } else if (operacao == 'W' && cache->politica_escrita == 0) {
+            (*escritas_memoria)++;
+        }
+
+        int linha_usar = (linha_vazia != -1) ? linha_vazia : linha_substituir;
+
+        if (cache->politica_escrita == 1 && conjunto->linhas[linha_usar].valido && conjunto->linhas[linha_usar].sujo) {
+            (*escritas_memoria)++;
+        }
+
+        conjunto->linhas[linha_usar].valido = 1;
+        conjunto->linhas[linha_usar].tag = tag;
+        conjunto->linhas[linha_usar].contador_lru = 0;
+        conjunto->linhas[linha_usar].contador_lfu = 1;
+        conjunto->linhas[linha_usar].sujo = (operacao == 'W') ? 1 : 0;
+    }
+
+    // Atualizar contadores LRU
+    for (int i = 0; i < cache->linhas_por_conjunto; i++) {
+        if (conjunto->linhas[i].valido) {
+            conjunto->linhas[i].contador_lru++;
+        }
+    }
+}
+
 Cache* inicializar_cache(int quantidade_conjuntos, int linhas_por_conjunto, int tamanho_linha, int politica_escrita, int tempo_acesso, int politica_substituicao) {
     Cache *cache = (Cache *)malloc(sizeof(Cache));
     cache->quantidade_conjuntos = quantidade_conjuntos;
@@ -87,6 +158,29 @@ void ler_parametros(int *politica_escrita, int *tamanho_linha, int *num_linhas, 
     arquivo_saida[strcspn(arquivo_saida, "\n")] = 0;
 }
 
+void processar_arquivo_entrada(char *arquivo_entrada, Cache *cache, int *total_enderecos, int *cont_leituras, int *cont_escritas, int *cont_acertos, int *cont_falhas, int *leituras_memoria, int *escritas_memoria) {
+    FILE *arquivo = fopen(arquivo_entrada, "r");
+    if (arquivo == NULL) {
+        perror("Erro ao abrir o arquivo de entrada");
+        exit(1);
+    }
+
+    char linha[100];
+    while (fgets(linha, sizeof(linha), arquivo)) {
+        uint32_t endereco;
+        char operacao;
+        sscanf(linha, "%x %c", &endereco, &operacao);
+        (*total_enderecos)++;
+        if (operacao == 'R') {
+            (*cont_leituras)++;
+        } else if (operacao == 'W') {
+            (*cont_escritas)++;
+        }
+        simular_cache(cache, endereco, operacao, cont_acertos, cont_falhas, leituras_memoria, escritas_memoria);
+    }
+    fclose(arquivo);
+}
+
 int main() {
 
     int politica_escrita, tamanho_linha, num_linhas, associatividade, tempo_acesso, tempo_leitura_memoria, tempo_escrita_memoria, politica_substituicao;
@@ -98,5 +192,9 @@ int main() {
     Cache *cache = inicializar_cache(quantidade_conjuntos, associatividade, tamanho_linha, politica_escrita, tempo_acesso, politica_substituicao);
 
     int total_enderecos = 0, cont_leituras = 0, cont_escritas = 0, cont_acertos = 0, cont_falhas = 0, leituras_memoria = 0, escritas_memoria = 0;
+
+    processar_arquivo_entrada(arquivo_entrada, cache, &total_enderecos, &cont_leituras, &cont_escritas, &cont_acertos, &cont_falhas, &leituras_memoria, &escritas_memoria);
+
+
 
 }
